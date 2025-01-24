83961144555059379e176b8f4d39dfae7f684ae8
Syed Wasif Ali <122001451+SyedWasifAli1@users.noreply.github.com>
2025-01-25 03:02:40 +0500

    Update firebase-config.ts

--- app/lib/firebase-config.ts
+++ app/lib/firebase-config.ts
@@ -4,14 +4,13 @@ import { getAuth } from 'firebase/auth';  // For Firebase Authentication
 import { getFirestore } from 'firebase/firestore';  // For Firestore database
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBeGl5FJE-ZkGeSJ2c_8BZZ67gXdTyLVeI",
-    authDomain: "authwithfirebase-20a41.firebaseapp.com",
-    databaseURL: "https://authwithfirebase-20a41-default-rtdb.firebaseio.com",
-    projectId: "authwithfirebase-20a41",
-    storageBucket: "authwithfirebase-20a41.appspot.com",
-    messagingSenderId: "513334310836",
-    appId: "1:513334310836:web:7090fa61046cd6f1dce4e1",
-    measurementId: "G-11T09DTR7V"
+  apiKey: "AIzaSyDJFZzM7hzFuak1L48aUykILnuK2_DT4Js",
+  authDomain: "bazaaristan-b4b6d.firebaseapp.com",
+  projectId: "bazaaristan-b4b6d",
+  storageBucket: "bazaaristan-b4b6d.firebasestorage.app",
+  messagingSenderId: "119813776331",
+  appId: "1:119813776331:web:55182171b8449a014b8c4e",
+  measurementId: "G-QPEBZL0276"
 };
 
 // Initialize Firebase if not already initialized
