982b53ee28086a253753172354be97a93cd5b4db
Syed Wasif Ali <122001451+SyedWasifAli1@users.noreply.github.com>
2025-01-28 02:41:17 +0500

    Update page.tsx

--- app/dashboard/order/page.tsx
+++ app/dashboard/order/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image';
 import { useEffect, useState } from "react";
-import * as XLSX from "xlsx"
+// import * as XLSX from "xlsx"
 import {
   collection,
   getDocs,
